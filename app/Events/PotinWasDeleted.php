bfd4a388efe416be8966b404faae3084898df530
Quentin Laplanche <quentin.laplanche@outlook.fr>
2017-06-10 15:30:18 +0200

    Events broadcasting to pusher debug console threw `php artisan potin:deleted {message}` works

---
+++ app/Events/PotinWasDeleted.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Events;
+
+use App\Post;
+use App\User;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class PotinWasDeleted implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $post;
+    public $user;
+    public $message;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($post,$user, $message)
+    {
+        $this->post = $post;
+        $this->user = $user; 
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
+    public function broadcastOn()
+    {
+        // return new PrivateChannel('channel-name');
+        return [
+            "potin-deleted.1"
+        ];
+    }
+}

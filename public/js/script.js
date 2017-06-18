281558577e7cf0ee0854322c20419adcb24088a5
jeterim <jeterim2@gmail.com>
2017-06-18 15:52:39 +0200

    Correction css

--- public/js/script.js
+++ public/js/script.js
@@ -2,37 +2,37 @@ jQuery(document).ready(function() {
     $('.vote_plus').on('click', function(event) {
         var post_id = $(this).parent().parent().data('id');
         $.ajax({
-		type:'POST',
-		url:'/vote',
-		data: {vote: "1", id: post_id},
-            	success: function(data) {
-			$('[data-id="'+data['id']+'"]').find("#accept-number").html(data['vote_accept']);
-			$('[data-id="'+data['id']+'"]').find("#decline-number").html(data['vote_decline']);
-			$('#credits').html(data['credits']);			
-            	},
-		error: function(data) {
-			console.log('Error: '+data);
-			console.log(data);
-		}
-	});
+            type: 'POST',
+            url: '/vote',
+            data: { vote: "1", id: post_id },
+            success: function(data) {
+                $('[data-id="' + data['id'] + '"]').find("#accept-number").html(data['vote_accept']);
+                $('[data-id="' + data['id'] + '"]').find("#decline-number").html(data['vote_decline']);
+                $('#credits').html(data['credits']);
+            },
+            error: function(data)  {
+                console.log('Error: ' + data);
+                console.log(data);
+            }
+        });
 
     });
 
     $('.vote_minus').on('click', function(event) {
         var post_id = $(this).parent().parent().data('id');
         $.ajax({
-		type:'POST',
-		url:'/vote',
-		data: {vote: "-1", id: post_id},
-            	success: function(data) {
-			$('[data-id="'+data['id']+'"]').find("#accept-number").html(data['vote_accept']);
-			$('[data-id="'+data['id']+'"]').find("#decline-number").html(data['vote_decline']);
-			$('#credits').html(data['credits']);
-            	},
-		error: function(data) {
-			console.log('Error: '+data);
-			console.log(data);
-		}
-	});
+            type: 'POST',
+            url: '/vote',
+            data: { vote: "-1", id: post_id },
+            success: function(data) {
+                $('[data-id="' + data['id'] + '"]').find("#accept-number").html(data['vote_accept']);
+                $('[data-id="' + data['id'] + '"]').find("#decline-number").html(data['vote_decline']);
+                $('#credits').html(data['credits']);
+            },
+            error: function(data)  {
+                console.log('Error: ' + data);
+                console.log(data);
+            }
+        });
     });
 });

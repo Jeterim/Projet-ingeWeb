951febbddd9142a730321f25b8affea5aacb2601
jeterim <jeterim2@gmail.com>
2017-06-18 15:27:18 +0200

    Rapide refonte css des notifications

--- resources/views/subviews/notification.blade.php
+++ resources/views/subviews/notification.blade.php
@@ -1,5 +1,8 @@
 <li>
-    <a href="#" id="{{$notification->id}}">{{$notification->data}}
+    <a href="#" id="{{$notification->id}}">
+        <span class="notif-time">{{$notification->created_at}}</span>
+        <span class="notif-title">Action</span>
+        <span class="notif-content">{{$notification->data}}</span>
         <span class="deletenotif glyphicon glyphicon-trash" data-id="{{$notification->id}}" aria-hidden="true"></span>
     </a>
 </li>
